7927f021c2177fcd575ccd1337cc7d16e36bacf6
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-10-02 11:38:42 +0700

    try using module pattern

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-let gameBoard = ["","","","","","","","",""]
-let winnerBool = 0
-let player1Turn = 1;
-let player2Turn = 0;
-let gameRun = 1;
-let divBody;
-let divResult;
-let resetBtn;
+const playerFactory = (name, mark) => {
+    
+    const sayHi = () => {
+        return `my name is ${name}!`
+    }
 
-function DOMGameBoard(){
+    const getMark = () => {
+        return mark;
+    }
+
+    const getName = () => {
+        return name;
+    }
+    
+    return { sayHi, getMark, getName };
+}
+
+
+
+
+const gameBoard = (() => {
+    
+    board = ["","","","","","","","",""]; //set as private , so have to use method to change the board state
+
+    const setBoard = (index, mark) => { //to set board
+        board[index] = mark;
+    } 
+
+    const getBoard = (index) => { //to set every cell in the DOM Board
+        return board[index];
+    }
+
+    const resetBoard = (index) => {
+        board[index] = ""
+    }
+
+    const currentBoardState = () => {
+        return board;
+    }
+
+    return {setBoard, getBoard, resetBoard, currentBoardState}
+})(); 
+
+
+
+const DOMGameBoard = (() => {
     let body = document.body;
     divBody = document.createElement('div');
-    let divChild;
     body.append(divBody);
     divBody.classList.add('div-body');
+    let divChild;
     for(let i = 0; i < 9; i++){
         divChild = document.createElement('div');
         // divChild.textContent = i;
@@ -27,96 +63,125 @@ function DOMGameBoard(){
     resetBtn = document.createElement('button');
     resetBtn.textContent = 'reset';
     body.append(resetBtn);
-}
 
-//check if player already win/not
-function winChecker(gameboard){
-    if ((gameboard[0] == 'x' && gameboard[1] == 'x' && gameboard[2] == 'x') ||
-        (gameboard[3] == 'x' && gameboard[4] == 'x' && gameboard[5] == 'x') ||
-        (gameboard[6] == 'x' && gameboard[7] == 'x' && gameboard[8] == 'x') ||
-        (gameboard[0] == 'x' && gameboard[3] == 'x' && gameboard[6] == 'x') ||
-        (gameboard[1] == 'x' && gameboard[4] == 'x' && gameboard[7] == 'x') ||
-        (gameboard[2] == 'x' && gameboard[5] == 'x' && gameboard[8] == 'x') ||
-        (gameboard[0] == 'x' && gameboard[4] == 'x' && gameboard[8] == 'x') ||
-        (gameboard[2] == 'x' && gameboard[4] == 'x' && gameboard[6] == 'x')) { 
-            winnerBool = 1;
-            gameRun = 0;
-            divResult.textContent = 'player1 win';
-    } else if
-        ((gameboard[0] == 'o' && gameboard[1] == 'o' && gameboard[2] == 'o') ||
-        (gameboard[3] == 'o' && gameboard[4] == 'o' && gameboard[5] == 'o') ||
-        (gameboard[6] == 'o' && gameboard[7] == 'o' && gameboard[8] == 'o') ||
-        (gameboard[0] == 'o' && gameboard[3] == 'o' && gameboard[6] == 'o') ||
-        (gameboard[1] == 'o' && gameboard[4] == 'o' && gameboard[7] == 'o') ||
-        (gameboard[2] == 'o' && gameboard[5] == 'o' && gameboard[8] == 'o') ||
-        (gameboard[0] == 'o' && gameboard[4] == 'o' && gameboard[8] == 'o') ||
-        (gameboard[2] == 'o' && gameboard[4] == 'o' && gameboard[6] == 'o')) {
-            winnerBool = 1;
-            gameRun = 0;
-            divResult.textContent = 'player2 win';
+    const getDivBody = () => {
+        return divBody;
     }
-}
 
-
-
-function drawChecker(gameboard){
-    if(gameboard[0]!="" &&
-        gameboard[1]!="" &&
-        gameboard[2]!="" &&
-        gameboard[3]!="" &&
-        gameboard[4]!="" &&
-        gameboard[5]!="" &&
-        gameboard[6]!="" &&
-        gameboard[7]!="" &&
-        gameboard[8]!="" &&
-        winnerBool == 0){
-            divResult.textContent = 'draw';
-            gameRun = 0;
-        }
-}
-
-const playerFactory = (name, mark) => {
-    return { name, mark };
-}
-
-const player1 = playerFactory('player1','x');
-const player2 = playerFactory('player2','o');
-
-
-
-DOMGameBoard();
-divBody.addEventListener('click',(event) => {
-    if(event.target.classList.contains('cell')) {
-        if(player1Turn === 1 && event.target.textContent === "" && winnerBool == 0){
-        gameBoard[event.target.getAttribute('data-value')] = player1.mark;
-        event.target.textContent = player1.mark;
-        player1Turn = 0;
-        player2Turn = 1;
-        winChecker(gameBoard);
-        drawChecker(gameBoard);
-    } else if(player2Turn === 1 && event.target.textContent === "" && winnerBool == 0){
-        gameBoard[event.target.getAttribute('data-value')] = player2.mark;
-        event.target.textContent = player2.mark;
-        player1Turn = 1;
-        player2Turn = 0;
-        winChecker(gameBoard);
-        drawChecker(gameBoard);
+    const getDivResult = () => {
+        return divResult;
     }
-}
-})
+
+    const getResetButton = () => {
+        return resetBtn;
+    }
+
+    return {getDivBody, getDivResult, getResetButton}
+})();
 
 
-// console.log(divBody.children[2])
-resetBtn.addEventListener('click', ()=> {
+
+const gameController = (() => {
+    let winnerBool = 0 //or can use round already 9 to check if draw
+    let player1Turn = 1;
+    let player2Turn = 0;
+    let gameRun = 1;
+    let divBody = DOMGameBoard.getDivBody(); 
+    let divResult = DOMGameBoard.getDivResult();
+    let resetBtn = DOMGameBoard.getResetButton();
+
+    const player1 = playerFactory('jeff','X');
+    const player2 = playerFactory('bob','O');
+
     
-    if(gameRun == 0){
-        for(let i = 0; i < divBody.children.length ; i++){
-            divBody.children[i].innerText = "";
+    
+    const winChecker = (gameboard) => {
+        if ((gameboard[0] == 'X' && gameboard[1] == 'X' && gameboard[2] == 'X') ||
+            (gameboard[3] == 'X' && gameboard[4] == 'X' && gameboard[5] == 'X') ||
+            (gameboard[6] == 'X' && gameboard[7] == 'X' && gameboard[8] == 'X') ||
+            (gameboard[0] == 'X' && gameboard[3] == 'X' && gameboard[6] == 'X') ||
+            (gameboard[1] == 'X' && gameboard[4] == 'X' && gameboard[7] == 'X') ||
+            (gameboard[2] == 'X' && gameboard[5] == 'X' && gameboard[8] == 'X') ||
+            (gameboard[0] == 'X' && gameboard[4] == 'X' && gameboard[8] == 'X') ||
+            (gameboard[2] == 'X' && gameboard[4] == 'X' && gameboard[6] == 'X')) { 
+                winnerBool = 1;
+                gameRun = 0;
+                divResult.textContent = player1.getName() + ' win';
+        } else if (
+            (gameboard[0] == 'O' && gameboard[1] == 'O' && gameboard[2] == 'O') ||
+            (gameboard[3] == 'O' && gameboard[4] == 'O' && gameboard[5] == 'O') ||
+            (gameboard[6] == 'O' && gameboard[7] == 'O' && gameboard[8] == 'O') ||
+            (gameboard[0] == 'O' && gameboard[3] == 'O' && gameboard[6] == 'O') ||
+            (gameboard[1] == 'O' && gameboard[4] == 'O' && gameboard[7] == 'O') ||
+            (gameboard[2] == 'O' && gameboard[5] == 'O' && gameboard[8] == 'O') ||
+            (gameboard[0] == 'O' && gameboard[4] == 'O' && gameboard[8] == 'O') ||
+            (gameboard[2] == 'O' && gameboard[4] == 'O' && gameboard[6] == 'O')) {
+                winnerBool = 1;
+                gameRun = 0;
+                divResult.textContent = player2.getName() + ' win';
         }
-        gameBoard = ["","","","","","","","",""];
-        winnerBool = 0;
-        player1Turn = 1;
-        player2Turn = 0;
-        gameRun = 1;
     }
-})
+
+    const drawChecker = (gameboard) => {
+        if (gameboard[0]!="" &&
+            gameboard[1]!="" &&
+            gameboard[2]!="" &&
+            gameboard[3]!="" &&
+            gameboard[4]!="" &&
+            gameboard[5]!="" &&
+            gameboard[6]!="" &&
+            gameboard[7]!="" &&
+            gameboard[8]!="" &&
+            winnerBool == 0) {
+                divResult.textContent = 'draw';
+                gameRun = 0;
+            }
+    }
+
+    //addEventListener isworking because already instantiated using IIFE
+    resetBtn.addEventListener('click', ()=> {
+    
+        if(gameRun == 0){
+            for(let i = 0; i < divBody.children.length ; i++){
+                gameBoard.resetBoard(i);
+                divBody.children[i].innerText = gameBoard.getBoard(i); // use the data from array as an anchor, 
+            }
+            divResult.textContent = ""
+            winnerBool = 0;
+            player1Turn = 1;
+            player2Turn = 0;
+            gameRun = 1;
+        }
+    })
+
+
+    divBody.addEventListener('click',(event) => {
+        console.log(event.target);
+        if(event.target.classList.contains('cell')) {
+            if(player1Turn === 1 && gameBoard.getBoard(event.target.getAttribute('data-value')) === "" && winnerBool === 0) {
+                gameBoard.setBoard(event.target.getAttribute('data-value'), player1.getMark());
+                event.target.textContent = gameBoard.getBoard(event.target.getAttribute('data-value'));
+                console.log(gameBoard.currentBoardState())
+                player1Turn = 0;
+                player2Turn = 1;
+                winChecker(gameBoard.currentBoardState());
+                drawChecker(gameBoard.currentBoardState());
+        } else if (player2Turn === 1 && gameBoard.getBoard(event.target.getAttribute('data-value')) === "" && winnerBool === 0){
+                gameBoard.setBoard(event.target.getAttribute('data-value'), player2.getMark());
+                event.target.textContent = gameBoard.getBoard(event.target.getAttribute('data-value'));
+                console.log(gameBoard.currentBoardState())
+                player1Turn = 1;
+                player2Turn = 0;
+                winChecker(gameBoard.currentBoardState());
+                drawChecker(gameBoard.currentBoardState());
+        }
+    }
+    })
+})()
+
+
+
+
+
+
+
